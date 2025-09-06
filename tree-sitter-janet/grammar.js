248897debad98797ad43ab9d59e65916661400a4
Tazmilur Saad <ssaad@colgate.edu>
2025-09-05 20:58:45 -0400

    Started tree-sitter grammar

---
+++ tree-sitter-janet/grammar.js
@@ -0,0 +1,26 @@
+/**
+ * @file Parser for the Janet language
+ * @author Tazmilur Saad <ssaad@colgate.edu>
+ * @license ISC
+ */
+
+/// <reference types="tree-sitter-cli/dsl" />
+// @ts-check
+
+module.exports = grammar({
+  name: "janet",
+
+  conflicts: $ => [
+    [$.value],
+  ],
+
+  rules: {
+    main: $ => repeat($.value), // root
+    value: $ => seq(repeat(choice($.ws, $.reader_mac)), $.raw_value, repeat($.ws)),
+    ws: $ => new RustRegex("[ \t\r\f\n\0\v]"), // whitespace
+    reader_mac: $ => new RustRegex("[';~,|]"), // reader macro
+    raw_value: $ => $.keyword,
+    symchars: $ => choice(new RustRegex('[a-zA-Z0-9\x80-\xFF]'), new RustRegex('[!$%&*+-./:<?=>@^_]')),
+    keyword: $ => seq(":", repeat1($.symchars)),
+  }
+});
