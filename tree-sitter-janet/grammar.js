924c14b6669e7ae48ee85df462f6fdf7c369db6d
Tazmilur Saad <ssaad@colgate.edu>
2025-09-05 22:04:54 -0400

    Added more tokens

--- tree-sitter-janet/grammar.js
+++ tree-sitter-janet/grammar.js
@@ -12,16 +12,59 @@ module.exports = grammar({
 
   conflicts: $ => [
     [$.value],
+    // [$.symbol],
+    // [$.keyword],
+    [$.many_symchars]
   ],
 
   rules: {
-    main: $ => repeat($.value), // root
-    value: $ => seq(repeat(choice($.ws, $.reader_mac)), $.raw_value, repeat($.ws)),
-    ws: $ => new RustRegex("[ \t\r\f\n\0\v]"), // whitespace
-    reader_mac: $ => new RustRegex("[';~,|]"), // reader macro
-    raw_value: $ => choice($.keyword, $.comment),
-    comment: _ => token(seq('#', /.*/)), // comments
-    symchars: $ => choice(new RustRegex('[a-zA-Z0-9\x80-\xFF]'), new RustRegex('[!$%&*+-./:<?=>@^_]')),
-    keyword: $ => seq(":", repeat1($.symchars)),
+    main: $ =>
+      repeat($.value), // root
+
+    value: $ =>
+      seq(repeat(choice($.ws, $.reader_mac)), $.raw_value, repeat($.ws)),
+
+    ws: $ =>
+      new RustRegex("[ \t\r\f\n\0\v]"), // whitespace
+
+    reader_mac: $ =>
+      new RustRegex("[';~,|]"), // reader macro
+
+    raw_value: $ =>
+      choice($.keyword, $.comment, $.symbol),
+
+    comment: _ =>
+      token(seq('#', /.*/)), // comments
+
+    constant: $ => "",
+
+    number: $ => "",
+
+    string: $ => "",
+
+    buffer: $ => "",
+
+    long_string: $ => "",
+
+    long_buffer: $ => "",
+
+    parray: $ => "",
+
+    barray: $ => "",
+
+    ptuple: $ => "",
+
+    btuple: $ => "",
+
+    struct: $ => "",
+
+    dict: $ => "",
+
+    symbol: $ => $.many_symchars,
+
+    many_symchars: $ => repeat1(choice(new RustRegex('[a-zA-Z0-9\x80-\xFF]'), new RustRegex('[!$%&*+-./:<?=>@^_]'))),
+
+    keyword: $ =>
+      seq(":", $.many_symchars),
   }
 });
---
+++ tree-sitter-janet/tests/symbols.janet
@@ -0,0 +1,3 @@
+first second third fourth
+5 6 7 8
+NINE9 XteN
